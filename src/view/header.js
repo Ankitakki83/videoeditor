005014d320ac59ffc7051946fe1868644f9adcb7
y-aakarsh <aakarsh@xelpmoc.in>
2017-12-09 11:54:52 +0530

    Adding Header component

---
+++ src/view/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./css/header.css";
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+        <div className="Header-text">Hello React</div>
+      </div>
+    );
+  }
+}
