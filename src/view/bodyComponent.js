79400ea673908c2c9d6bff30acbf403bed163214
ankitakki83 <mauryaankit83@gmail.com>
2017-12-10 09:22:24 -0800

    Addin item in left ccomponetn

--- src/view/bodyComponent.js
+++ src/view/bodyComponent.js
@@ -5,9 +5,37 @@ export default class BodyComponent extends React.Component {
   render() {
     return (
       <div className="BodyComponent">
-        <div className="BodyComponent-left">left body is here</div>
-        <div className="BodyComponent-right">right body is here</div>
+        <div className="BodyComponent-left">left body is here
+
+        <ul className="BodyCompoent-leftList"> 
+          <li> first item</li>
+          <li> second item</li>
+          <li> third item</li>
+          <li> fourth item</li>
+
+
+          </ul>
+
+        
+        
+        
+        
+        </div>
+        <div className="BodyComponent-right">right body is here
+        <ol>
+
+        <li>fourth item </li>
+        <li>  fourth item</li>
+        <li> fourth item </li>
+        <li> fourth item</li>
+        <li> fourth item </li>
+
+        </ol>
+        
+        </div>
       </div>
     );
   }
 }
+
+
